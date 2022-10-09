2c49846b333e31404072853728426e1611cfb2ad
Kate <cvetok_spb@bk.ru>
2022-10-09 10:46:05 +0300

    Задача-2

---
+++ 2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine ("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int sum = 0;
+while (N>0)
+{
+sum = sum + N % 10;
+N = N/10;
+}
+
+    Console.WriteLine($"Сумма цифр в числе равно {sum}");
